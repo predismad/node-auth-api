add1cd88261d396d93cb66c14982ae426b45a5c3
predismad <mirkogrz@gmail.com>
2022-05-17 11:48:00 +0200

    added comment

--- helpers/checkAdminStatus.js
+++ helpers/checkAdminStatus.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const ADMIN_MAILS = process.env.ADMIN_MAILS.split(', ');
 
+// CHECKS IF EMAIL FROM USER IS IN ADMIN_MAILS
 exports.checkAdminStatus = (email) => {
     return ADMIN_MAILS.includes(email.toLowerCase());
 };
