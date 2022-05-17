ae32091c0146ebb8f427500c664e06a6ad2e1560
predismad <mirkogrz@gmail.com>
2022-05-17 11:33:07 +0200

    removed 1 from key variable

--- helpers/generateKey.js
+++ helpers/generateKey.js
@@ -1,4 +1,4 @@
 const crypto = require('crypto')
 
-const key1 = crypto.randomBytes(32).toString('hex')
-console.table({ key1 })
+const key = crypto.randomBytes(32).toString('hex')
+console.table({ key })
