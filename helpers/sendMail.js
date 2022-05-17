9773b668d60a484c5207a919700d92199cd7050f
predismad <mirkogrz@gmail.com>
2022-05-17 11:47:04 +0200

    added comments

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -3,14 +3,17 @@ const nodemailer = require('nodemailer');
 const sgTransport = require('nodemailer-sendgrid-transport');
 const template = require('./email-templates/template');
 
+// AUTH OPTIONS FOR E-MAIL SERVICE
 var options = {
     auth: {
       api_key: process.env.SENDGRID_API_KEY
     }
 }
 
+// CREATE NODEMAILER CLIENT WITH OPTIONS
 var client = nodemailer.createTransport(sgTransport(options));
 
+// SEND PASSWORD RESET EMAIL WITH LINK TO RESET PASSWORD
 exports.sendPasswordResetMail = async (to, resetToken) => {
   const resetLink = `${process.env.FRONTEND_URL}/reset-password/${resetToken}`;
   var email = {
@@ -22,6 +25,7 @@ exports.sendPasswordResetMail = async (to, resetToken) => {
   await sendMail(email);
 }
 
+// SEND ACTIVATION EMAIL WITH LINK TO ACTIVATE ACCOUNT
 exports.sendAccountActivationMail = async (to, activationToken) => {
   const activationLink = `${process.env.FRONTEND_URL}/api/user/activate/${activationToken}`;
   var email = {
@@ -33,6 +37,7 @@ exports.sendAccountActivationMail = async (to, activationToken) => {
   await sendMail(email);
 }
 
+// SENDS MAIL WITH GIVEN EMAIL OBJECT
 async function sendMail(email) {
   await client.sendMail(email, function(err, info) {
     if (err ) {
